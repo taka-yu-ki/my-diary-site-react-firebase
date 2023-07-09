d0e9a420d568ff6a600d7109c59c5d51355bec2e
たか <taka7_zegs@icloud.com>
2023-07-09 17:28:50 +0900

    sixth-commit 20230709

--- src/components/CalenderFunction.js
+++ src/components/CalenderFunction.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import Calender from "react-calendar";
 import "../Calendar.css";
 import { useNavigate } from "react-router-dom";
+// import { db } from "../firebase";
+// import { collection, getDocs } from "firebase/firestore";
 
 const CalenderFunction = () => {
   const [value, onChange] = useState(new Date());
   const navigate = useNavigate();
+
+  // const [diaries, setDiaries] = useState([]);
+
+  // useEffect(() => {
+  //   const diaryDate = collection(db, "diaries");
+  //   getDocs(diaryDate).then((snapShot) => {
+  //     setDiaries(snapShot.docs.map((doc) => ({ ...doc.data() })));
+  //   });
+  // });
 
   const handleButtonClick = () => {
     navigate("/diaryPage");
