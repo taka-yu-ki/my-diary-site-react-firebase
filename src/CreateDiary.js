ac0d5e85a6f4e556ce8766f2020b7c8642c87b29
たか <taka7zegs@gmail.com>
2023-07-24 14:59:43 +0900

    10th-commit 20230724

--- src/CreateDiary.js
+++ src/CreateDiary.js
@@ -1,40 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { auth, db } from "./firebase";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc, deleteDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const CreateDiary = () => {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [isEditMode, setIsEditMode] = useState(false);
   const navigate = useNavigate();
 
+  // ルート元から特定のデータを受け取り、変数に格納する処理
   const date = useLocation().state.date;
-  const formattedDate = date.toISOString().slice(0, 10).replace(/-/g, "");
+  const diary = useLocation().state.diary;
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  const id = `${year}${month}${day}`;
 
+  const userId = auth.currentUser.uid;
+  const ref = doc(db, `users/${userId}/diaries/${id}`);
+
+  // 文字数制限の変数
+  const maxTitleLength = 12;
+  const maxTextLength = 500;
+
+  // firebaseから既存のデータを取得し、編集モードに変更する処理
+  useEffect(() => {
+    const fetchDiaryData = async () => {
+      try {
+        const userId = auth.currentUser.uid;
+        const ref = doc(db, `users/${userId}/diaries/${id}`);
+        const snapshot = await getDoc(ref);
+        if (snapshot.exists()) {
+          setIsEditMode(true);
+          setTitle(diary.title);
+          setText(diary.text);
+        }
+      } catch (error) {
+        console.error("データの取得に失敗しました:", error);
+      }
+    };
+    fetchDiaryData();
+  }, [id, diary]);
+
+  // 日記を追加・更新する処理
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const userId = auth.currentUser.uid;
-    const ref = doc(db, `users/${userId}/diaries/${formattedDate}`);
     const data = {
       date: date,
       title: title,
       text: text,
     };
-
     try {
       await setDoc(ref, data);
-      alert("日記が追加されました！");
+      alert(`日記が${isEditMode ? "更新" : "追加"}されました！`);
+      setIsEditMode(false);
       navigate("/");
     } catch (error) {
       alert(error.message);
     }
   };
 
+  const deletePost = () => {
+    return deleteDoc(ref).then(async () => {
+      alert("記事を削除しました");
+
+      navigate("/");
+    });
+  };
+
   return (
     <form className="diary-form" onSubmit={handleSubmit}>
-      <h2>日記作成</h2>
+      <h2> {isEditMode ? "日記更新" : "日記作成"}</h2>
       <div className="form-group">
         <label htmlFor="title">タイトル</label>
         <input
@@ -43,7 +82,12 @@ const CreateDiary = () => {
           className="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          maxLength={maxTitleLength}
+          required
         />
+        <span className="char-count">
+          {`${title.length} / ${maxTitleLength}文字`}
+        </span>
       </div>
       <div className="form-group">
         <label htmlFor="content">内容</label>
@@ -53,9 +97,31 @@ const CreateDiary = () => {
           rows="5"
           value={text}
           onChange={(e) => setText(e.target.value)}
+          maxLength={maxTextLength}
+          required
         ></textarea>
+        <span className="char-count">
+          {`${text.length} / ${maxTextLength}文字`}
+        </span>
       </div>
-      <button type="submit">保存</button>
+      <div className="button-group">
+        <button className="button submit" type="submit">
+          {isEditMode ? "更新" : "保存"}
+        </button>
+        {isEditMode && (
+          <button className="button delete" type="button" onClick={deletePost}>
+            削除
+          </button>
+        )}
+
+        <button
+          className="button back"
+          type="button"
+          onClick={() => navigate("/")}
+        >
+          戻る
+        </button>
+      </div>
     </form>
   );
 };
--- src/diaryPage.js
+++ src/diaryPage.js
@@ -1,13 +1,32 @@
 import React from "react";
 import "./App.css";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const DiaryPage = () => {
+  const navigate = useNavigate();
   const diary = useLocation().state.diary;
+  const date = useLocation().state.date;
   return (
     <div className="diary">
       <h2 className="diary-title">{diary.title}</h2>
       <p className="diary-text">{diary.text}</p>
+      <div className="button-group">
+        <button
+          className="button edit"
+          onClick={() =>
+            navigate("/CreateDiary", { state: { date: date, diary: diary } })
+          }
+        >
+          編集
+        </button>
+        <button
+          className="button back"
+          type="button"
+          onClick={() => navigate("/")}
+        >
+          戻る
+        </button>
+      </div>
     </div>
   );
 };
